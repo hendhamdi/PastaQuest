7ab8e38b1e1cdc5a5e71e0c8212ec29988e234ad
unknown <hendh0487@gmail.com>
2025-04-24 16:35:10 +0100

    Remove .history

--- src/components/ReservationInfo/ReservationInfo.jsx
+++ src/components/ReservationInfo/ReservationInfo.jsx
@@ -6,13 +6,13 @@ const ReservationInfo = () => {
     <div className="reservation-info-container">
       <div className="info-section">
         <h2>Contactez-nous :</h2>
-        <p className='num-tel'>+216-234-567-8900</p>
+        <p className='num-tel'>+216 1 23 45 67 89</p>
       </div>
       <div className="info-section">
         <h2>Heures d'ouverture :</h2>
         <p>
-          Du lundi au vendredi :<br/> 11:00h - 00:00h <br />
-          Le samedi et dimanche : <br/>10:00 - 01:00
+          Du lundi au vendredi :<br/> 11h00 - 00h00 <br />
+          Le samedi et dimanche : <br/>10h00 - 01h00
         </p>
       </div>
       <div className="info-section">
